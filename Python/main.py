43cc8f01bd019eae6af3cb5ce80da3208324ede1
Miriam <you@someplace.com>
2015-09-08 18:44:48 +0300

    move to stam web

--- Python/main.py
+++ Python/main.py
@@ -3,16 +3,24 @@
 import cameraRead
 import faceDetect
 import os
+import time
 
-file = 'image.png'
-cameraRead.read_image(file)
-detected = faceDetect.face_detect(file)
+file = '/usr/lib/edison_config_tools/public/image.png'
 
-if (detected != None):
+while (1 > 0):
+	cameraRead.read_image(file)
+	detected = faceDetect.face_detect(file)
+
+	if (detected != None):
+		interact()
+		time.sleep(30)
+		
+	
+
+def interact():
 	to_say = "Nice to meet you. Would you like a candy"
-        cm = 'espeak "'+to_say+'"'
-        os.system(cm)
-
+    cm = 'espeak "'+to_say+'"'
+    os.system(cm)
 
 
 
