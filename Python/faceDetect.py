94f75c2c002e8231fe220a32fc391d6b4592f316
Miriam <you@example.com>
2015-09-08 13:01:13 +0000

    checks pic and talks

--- Python/faceDetect.py
+++ Python/faceDetect.py
@@ -2,12 +2,13 @@
 import numpy
 import cv2
 import sys
-from espeak import espeak
+import os
 
 
 def face_detect(in_file):
-	img = cv2.imread(infile)
-	out_file = "{0}.detected".format(in_file)
+	img = cv2.imread(in_file)
+        filename, file_extension = os.path.splitext(in_file)
+	out_file = "{0}.detected.{1}".format(filename, file_extension)
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 	faceCascade = cv2.CascadeClassifier('/usr/lib/edison_config_tools/public/haarcascade_frontalface_alt.xml')
@@ -20,10 +21,9 @@ def face_detect(in_file):
 		return None
 		
 	if num_of_faces > 1:
-
-	    to_say = Too many people. One at a time please!"
-        cm = 'espeak "'+to_say+'"'
-        os.system(cm)
+        	to_say = "Too many people. One at a time please!"
+        	cm = 'espeak "'+to_say+'"'
+        	os.system(cm)
 		#espeak.synth("")
 		return None
 		
@@ -31,5 +31,5 @@ def face_detect(in_file):
 	  cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
 	cv2.imwrite(out_file,img)
-	return face[0];
+	return faces[0];
 
--- Python/main.py
+++ Python/main.py
@@ -2,10 +2,17 @@
 
 import cameraRead
 import faceDetect
+import os
 
 file = 'image.png'
 cameraRead.read_image(file)
-faceDetect.face_detect(file)
+detected = faceDetect.face_detect(file)
+
+if (detected != None):
+	to_say = "Nice to meet you. Would you like a candy"
+        cm = 'espeak "'+to_say+'"'
+        os.system(cm)
+
 
 
 
